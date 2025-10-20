d3438640e453aba7b2b33e9c8900688d9fbe0d5c
Abusolay <sefat01625@gmail.com>
2025-10-20 18:52:15 +0600

    volunteer top

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["https://volunteer-management-56adc.web.app"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
